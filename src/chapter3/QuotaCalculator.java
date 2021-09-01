79a79028444b6de4eef9e4626c462de57cf68a3a
Theresa <theresaobamwonyi@gmail.com>
2021-09-01 10:11:36 +0100

    if else statements

--- src/chapter3/QuotaCalculator.java
+++ src/chapter3/QuotaCalculator.java
@@ -1,5 +1,26 @@
 package chapter3;
 
-public class QuotaCalculator {
+/*
+If else
+ */
 
+import java.util.Scanner;
+
+public class QuotaCalculator {
+    public static void main(String args[]){
+        int quota = 10;
+
+        System.out.println("Enter the number of sales you made this week: ");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        if(sales >= quota){
+            System.out.println("Congratulations!! You met your quota.");
+        }
+        else{
+            int saleShort = quota-sales;
+            System.out.println("You did not meet your quota. You were "+saleShort+ " sales short.");
+        }
+    }
 }
