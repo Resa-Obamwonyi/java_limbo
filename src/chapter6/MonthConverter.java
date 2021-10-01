6cc74b70dce55509cbace6748b223cc3af1f0e12
Theresa <theresaobamwonyi@gmail.com>
2021-10-01 23:02:29 +0100

    class and objects

---
+++ src/chapter6/MonthConverter.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class MonthConverter {
+    public static void main (String args[]){
+        System.out.println(Month.getMonth(2));
+        System.out.println(Month.getMonth("April"));
+    }
+}
